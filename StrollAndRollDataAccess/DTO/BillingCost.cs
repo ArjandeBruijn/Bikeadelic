2b432740f197a5a14a4ea1bb0d9bdb816a36dcb2
Arjan de Bruijn <amgdebruijn@gmail.com>
2019-08-25 20:30:00 -0600

    added missing files

---
+++ StrollAndRollDataAccess/DTO/BillingCost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrollAndRollDataAccess
+{
+    public class BillingCost
+    {
+        public double Price { get; set; } = 0;
+        public double Tax { get; set; } = 0;
+        public double Discount { get; set; } = 0;
+    }
+}
---
+++ StrollAndRollDataAccess/DTO/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrollAndRollDataAccess
+{
+    public class Event
+    {
+        public string ID { get; set; }
+
+        public DayPartSelection.DayPart DayPart { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Label { get; set; }
+    }
+}
