661af27c2c86b92f72af5c5674064b47b8ce1599
Arjan_de_Bruijn <amgdebruijn@gmail.com>
2019-08-25 23:36:27 -0600

    Addded UploadPicsVids.csproj

---
+++ UploadPicsVids/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace UploadPicsVids
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textboxFeedBack = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textboxFeedBack
+            // 
+            this.textboxFeedBack.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textboxFeedBack.Location = new System.Drawing.Point(0, 0);
+            this.textboxFeedBack.Name = "textboxFeedBack";
+            this.textboxFeedBack.Size = new System.Drawing.Size(698, 450);
+            this.textboxFeedBack.TabIndex = 0;
+            this.textboxFeedBack.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textboxFeedBack);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textboxFeedBack;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UploadPicsVids/Form1.cs
@@ -0,0 +1,106 @@
+﻿using Dropbox;
+using Dropbox.Api.Sharing;
+using StrollAndRollDataAccess;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UploadPicsVids
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void LogMessage(string msg)
+        {
+            if (textboxFeedBack.Text.Length > 0)
+            {
+                textboxFeedBack.Text += Environment.NewLine;
+            }
+            textboxFeedBack.Text += msg;
+
+            textboxFeedBack.SelectionStart = textboxFeedBack.Text.Length;
+
+            textboxFeedBack.ScrollToCaret();
+        }
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            string accessToken = "Lzk0cOUr-BoAAAAAAABn1QgKontvLa2ma0vtCU24NTo9vEeBTeFSjiROUL5rY_6F";
+            string appKey = "e7852uopirsqcja";
+            string appSecret = "dvhf2ijy2z99ckg";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string fileName in openFileDialog1.FileNames)
+                {
+                    LogMessage(fileName);
+                     
+                    Dropbox.DropboxClient client = new Dropbox.DropboxClient(appKey, appSecret);
+
+                    if (Path.GetExtension(fileName) == ".mp4")
+                    {
+                        LogMessage($"Cannot process mp4 file {fileName}");
+
+                        continue;
+                    }
+
+                    string fileNameOnDropbox = Path.GetFileName(fileName);
+
+                    UploadFileRequest request = new UploadFileRequest(fileNameOnDropbox);
+
+                    Image image = Image.FromFile(fileName);
+
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        image.Save(stream, image.RawFormat);
+
+                        stream.Position = 0;
+
+                        await client.UploadFileAsync(request, stream, accessToken);
+                    }
+
+                    using (var apiClient = new Dropbox.Api.DropboxClient(accessToken))
+                    {
+
+                        string fn = $"/{fileNameOnDropbox}";
+
+                        ListSharedLinksResult sharedLinksResult = await apiClient.Sharing.ListSharedLinksAsync(new ListSharedLinksArg(fn));
+
+                        string url = string.Empty;
+
+                        if (sharedLinksResult.Links.Count > 0)
+                        {
+                            url = sharedLinksResult.Links[0].Url;
+                        }
+                        else
+                        {
+                            SharedLinkMetadata sharedLinkMetaData = await apiClient.Sharing.CreateSharedLinkWithSettingsAsync(fn);
+
+                            url = sharedLinkMetaData.Url;
+
+                            //DatabaseOperations.
+                        }
+
+                        textboxFeedBack.Text += $"Url = {url}";
+                    }
+
+                }
+
+                 
+            }
+
+        }
+
+    }
+}
